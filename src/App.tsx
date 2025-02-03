188380e1b33af904d34e8dde249c3103f8e6e03a
Emmanuel Ochieng <eochieng16@@gmail.com>
2025-02-03 14:43:49 +0300

    updated home page banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import { Link } from "react-router"
+import { motion } from "framer-motion"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-[100vh] bg-red-200">
+          <nav className="mt-[25px] inline-flex justify-center w-full">
+            <div className="h-[82.75px] w-[80%] rounded-[64px] bg-white flex items-center justify-between px-[25px] shadow">
+                <img src="/logo.svg" alt="Logo" className="h-[42.75px] w-[150px]" />
+                <div className="w-[40%] flex justify-between font-light text-[16px]">
+                  <Link to={`/`}>Home</Link>
+                  <Link to={`/`}>Property Search</Link>
+                  <Link to={`/`}>Our Services</Link>
+                  <Link to={`/`}>About Us</Link>
+                  <Link to={`/`}>Blogs</Link>
+                </div>
+                <div className="flex items-center justify-between w-[10%]">
+                  <img src="/call.svg" alt="Logo" className="h-[20px] w-[20px]" />
+                  <p className="text-[16px]">+254712151558</p>
+                </div>
+                <motion.button className="border-[1px] border-slate-400 h-[42.5px] w-[148.22px]">Contact Us</motion.button>
+            </div>
+          </nav>
       </div>
-      <h1 className='bg-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
