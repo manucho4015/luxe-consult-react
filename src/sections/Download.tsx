05e08d3f1036290145571c7f53ee4de5315ba7f3
Emmanuel Ochieng <eochieng16@gmail.com>
2025-02-07 18:37:45 +0300

    add bottom margin

--- src/sections/Download.tsx
+++ src/sections/Download.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 const Download = () => {
     return (
         <div className="mt-[25px] h-[30vh] min-h-[176px] inline-flex w-full justify-center items-center text-white
-            bg-linear-to-r from-[#EEC7A2] to-[#000]">
+            bg-linear-to-r from-[#EEC7A2] to-[#000] mb-[25px]">
             <h3 className="uppercase mr-[30px]">download our company profile</h3>
             <motion.button whileTap={{ scale: .95 }} className='uppercase bg-primary rounded-md w-[200px] h-[50px]
                     flex items-center justify-center text-[16px]'>
