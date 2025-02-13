87b19038ed2b1698cf7afa7e4f6f08ffa7f719b5
Emmanuel Ochieng <eochieng16@gmail.com>
2025-02-13 14:25:00 +0300

    responsiveness -> leadership team

--- src/pages/team/components/TeamMember.tsx
+++ src/pages/team/components/TeamMember.tsx
@@ -2,7 +2,7 @@
 
 const TeamMember = () => {
     return (
-        <div className='bg-white inline-block rounded-2xl p-[15px] max-w-[374px] max-h-[443pxpx] w-[20vw] h-[60vh] shadow'>
+        <div className='bg-white inline-block rounded-2xl p-[15px] max-w-[374px]  lg:max-h-[443pxpx] w-full md:w-[40vw] lg:w-[20vw] lg:h-[60vh] shadow'>
             {/* image */}
             <div className="bg-red-200 rounded-2xl p-[10px]  max-h-[296.27px] w-full h-[55vh]">
             </div>
