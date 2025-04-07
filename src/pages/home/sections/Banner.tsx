f0ef12628a1f57af0ffa497a3d225097a3bdb24a
Emmanuel Ochieng <eochieng16@gmail.com>
2025-04-07 11:39:11 +0300

    mobile responsiveness

--- src/pages/home/sections/Banner.tsx
+++ src/pages/home/sections/Banner.tsx
@@ -15,8 +15,8 @@ const Banner = () => {
     return (
         <div className="min-h-[100vh] bg-[url(/beautiful-architecture-building.jpg)] bg-cover bg-center bg-blend-color bg-[#00000070] relative flex items-center justify-center">
             <div className="w-[75%]">
-                <h2 className='capitalize text-primary font-semibold text-[90px] text-center'>search. see. love</h2>
-                <p className="text-white text-center text-[22px]">Our mission is to make your real estate journey seamless, personalized and enjoyable, ensuring our clients achieve their real estate goals with ease and eficiency.</p>
+                <h2 className='capitalize text-primary font-semibold text-[40px] md:text-[90px] text-center'>search. see. love</h2>
+                <p className="text-white text-center text-[16px] md:text-[22px]">Our mission is to make your real estate journey seamless, personalized and enjoyable, ensuring our clients achieve their real estate goals with ease and eficiency.</p>
             </div>
 
             <div className="absolute bottom-10 flex flex-col w-full items-center">
