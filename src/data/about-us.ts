c617e45b12ec07c91cec458bf11d6a8c659aeac4
Emmanuel Ochieng <eochieng16@gmail.com>
2025-05-01 14:57:22 +0300

    final changes - 3

--- src/data/about-us.ts
+++ src/data/about-us.ts
@@ -3,24 +3,18 @@ export const elements = [
         img_src: '/rocket.svg',
         alt: 'mission',
         title: 'our mission',
-        description: 'Our mission is to empower individuals and businesses to achieve their real estate goals with integrity, innovation, and personalized service.'
+        description: `We are dedicated to understanding the unique needs of each client, providing timely solutions, and ensuring the highest standards of service in every transaction.`
     },
     {
         img_src: '/human-world-network.svg',
         alt: 'vision',
         title: 'our vision',
-        description: 'Our mission is to empower individuals and businesses to achieve their real estate goals with integrity, innovation, and personalized service.'
+        description: `To be the leading provider of luxury real estate solutions in Nairobi, known for our commitment to excellence, personalized service, and transformative property experiences.`
     },
     {
         img_src: '/gold-bars.svg',
         alt: 'values',
         title: 'our values',
         description: 'Our mission is to empower individuals and businesses to achieve their real estate goals with integrity, innovation, and personalized service.'
-    },
-    {
-        img_src: '/accounts-group.svg',
-        alt: 'spirit',
-        title: 'our spirit',
-        description: 'Our mission is to empower individuals and businesses to achieve their real estate goals with integrity, innovation, and personalized service.'
-    },
+    }
 ]
