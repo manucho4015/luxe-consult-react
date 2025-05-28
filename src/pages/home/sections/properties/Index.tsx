c242f9612439535195c55aa74f7ac0ff7011c845
Emmanuel Ochieng <eochieng16@gmail.com>
2025-05-28 16:34:25 +0300

    enable filtering featured properties

--- src/pages/home/sections/properties/Index.tsx
+++ src/pages/home/sections/properties/Index.tsx
@@ -4,22 +4,39 @@ import { motion } from 'framer-motion'
 // components
 import PropertyCard from './components/Property'
 
-// data
-import { selectToggles } from '../../../../data/home'
-
 // api
 import { fetchProperties } from '../../../../api/properties'
+import { fetchPropertyTypes } from '../../../../api/property-types'
 
 // custom types
 import type { Property } from '../../../../custom-types/properties'
+import type { PropertyType } from '../../../../custom-types/property-types'
 
 const Properties = () => {
     const [isActive, setIsActive] = useState('')
+    const [propertyTypes, setPropertyTypes] = useState<PropertyType[]>([])
     const [properties, setProperties] = useState<Property[]>([])
+    const [filterProperties, setFilterProperties] = useState<Property[]>([])
+
+
+    const handleFilter = (type: string) => {
+        if (isActive === type) {
+            setIsActive('')
+            setFilterProperties(properties)
+        } else {
+
+            setIsActive(type)
+            setFilterProperties(properties.filter((property) => property.property_type.name === type))
+        }
+    }
 
     useEffect(() => {
         fetchProperties().then((response) => {
             setProperties(response.filter((property) => property.category.toUpperCase() === 'FEATURED'))
+            setFilterProperties(response.filter((property) => property.category.toUpperCase() === 'FEATURED'))
+        })
+        fetchPropertyTypes().then((response) => {
+            setPropertyTypes(response)
         })
     }, [])
     return (
@@ -28,13 +45,13 @@ const Properties = () => {
             <p className=" text-[17px] text-center">Partner With Us for Seamless Real Estate Success</p>
             <div className="inline-flex mt-[25px] justify-center w-full">
                 {/* select toggle */}
-                <div className="flex flex-wrap lg:justify-between w-[90vw]">
+                <div className="flex flex-wrap gap-1  w-[90vw]">
                     {
-                        selectToggles.map((toggle) => (
-                            <motion.div animate={{ backgroundColor: isActive == toggle.value ? '#000' : '#E9E9E9', color: isActive == toggle.value ? '#fff' : '#000' }}
+                        propertyTypes.map((type, index) => (
+                            <motion.div key={index} animate={{ backgroundColor: isActive == type.name ? '#000' : '#E9E9E9', color: isActive == type.name ? '#fff' : '#000' }}
                                 className={`min-h-[53px] min-w-[141px] px-[15px] mr-[10px] mb-[10px] lg:mr-0 cursor-default text-[18px] flex items-center justify-center  rounded-xl capitalize `}
-                                onClick={() => setIsActive(toggle.value)}>
-                                {toggle.label}
+                                onClick={() => handleFilter(type.name)}>
+                                {type.name}
                             </motion.div>
                         ))
                     }
@@ -43,7 +60,7 @@ const Properties = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 px-[5vw] md:px-[2.5vw] gap-10 mt-[50px]">
                 {
-                    properties.map((property, index) => (
+                    filterProperties.map((property, index) => (
                         <PropertyCard key={index} property={property} />
                     ))
                 }
