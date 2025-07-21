450d5b6eba481c8c766b12b3dd8b3047f823bec0
Emmanuel Ochieng <eochieng16@gmail.com>
2025-07-21 14:13:58 +0300

    sort properties on most popular cities

--- src/pages/home/sections/Popular.tsx
+++ src/pages/home/sections/Popular.tsx
@@ -1,23 +1,39 @@
+import { Link } from "react-router"
 
-import { categories } from "../../../data/categories"
+// custom types
+import type { Property } from "../../../custom-types/properties"
 
-const Categories = () => {
+const Categories = ({ properties }: { properties: Property[] }) => {
+
+    const grouped: Record<string, number> = {}
+
+    properties.forEach(property => {
+        const key = property.location.city
+        grouped[key] = (grouped[key] || 0) + 1
+    })
+    let popularPlaces = Object.entries(grouped).map(([city, count]) => (
+        { city, count }
+    ))
+
+    popularPlaces = popularPlaces.sort((a, b) => b.count - a.count)
 
     return (
         <div className='min-h-[50vh] inline-block w-full'>
             <h2 className="font-bold text-[40px] mt-[100px] text-center capitalize">most popular places</h2>
-            <div className="flex flex-wrap justify-center lg:justify-between px-[10vw] mt-[25px]">
+            <div className="flex flex-wrap gap-8 px-[10vw] mt-[25px]">
                 {
-                    categories.map((category, index) => (
-                        <div key={index} className={`${category.img_src ? category.img_src : 'bg-[url(/city.jpg)]'} max-w-[256px] max-h-[338.11px] h-[50vh] w-[30vh] bg-blend-color bg-[#00000060] rounded-md p-[20px] shadow text-white lg:mb-0 lg:mr-0 mb-[20px] mr-[20px] bg-center bg-cover`}>
-                            <div className="inline-flex w-full justify-center">
-                                <div className=" rounded-2xl border-primary border-[1px] min-w-[50%] p-[10px] bg-[#E5AC7410]">
-                                    <h4 className="font-medium text-[19px]">{category.title}</h4>
-                                    <p className="font-light text-[15px]">{category.paragraph}</p>
+                    popularPlaces.map((place, index) => (
+                        <Link to={`property-search?location=${place.city}`}>
+                            <div key={index} className={`bg-[url(/modern-residential-building.jpg)] max-w-[256px] max-h-[338.11px] h-[50vh] w-[30vh] bg-blend-color bg-[#00000060] rounded-md p-[20px] shadow text-white lg:mb-0 lg:mr-0 mb-[20px] mr-[20px] bg-center bg-cover`}>
+                                <div className="inline-flex w-full justify-center">
+                                    <div className=" rounded-2xl border-primary border-[1px] min-w-[50%] p-[10px] bg-[#E5AC7410]">
+                                        <h4 className="font-medium text-[19px]">{place.city}</h4>
+                                        <p className="font-light text-[15px]">{place.count} {place.count == 1 ? 'property' : 'properties'}</p>
+                                    </div>
                                 </div>
-                            </div>
 
-                        </div>
+                            </div>
+                        </Link>
                     ))
                 }
             </div>
